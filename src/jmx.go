e3cdbea4364dbc3156ee61dcf66d8648b5319748
Camden Cheek <camden.cheek@bluemedora.com>
2018-08-13 11:05:44 -0400

    Add exit when invalid filepath

--- src/jmx.go
+++ src/jmx.go
@@ -64,6 +64,7 @@ func main() {
 		// Check that the filepath is an absolute path
 		if !filepath.IsAbs(collectionFile) {
 			log.Error("Invalid metrics colleciton path %s. Metrics collection files must be specified as absolute paths.", collectionFile)
+			os.Exit(1)
 		}
 
 		// Parse the yaml file into a raw definition
