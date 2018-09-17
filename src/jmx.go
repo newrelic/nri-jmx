f822f2cd4eacb3f60f33b4943023ccc207c40e7a
Corbin Phelps <corbin.phelps@bluemedora.com>
2018-09-17 09:04:51 -0400

    Added soft limit for entity metric count (#21)
    * Added soft limit for entity metric count
    
    * Made the description of the MetricLimit param clearer
    
    * Changed behavior for metric limit. If violated now the offending entity will not be reported.

--- src/jmx.go
+++ src/jmx.go
@@ -19,11 +19,12 @@ type argumentList struct {
 	JmxPass         string `default:"admin" help:"The password for the JMX connection"`
 	CollectionFiles string `default:"" help:"A comma separated list of full paths to metrics configuration files"`
 	Timeout         int    `default:"10000" help:"Timeout for JMX queries"`
+	MetricLimit     int    `default:"200" help:"Number of metrics that can be collected per entity. If this limit is exceeded the entity will not be reported. A limit of 0 implies no limit."`
 }
 
 const (
 	integrationName    = "com.newrelic.jmx"
-	integrationVersion = "0.1.1"
+	integrationVersion = "0.1.2"
 )
 
 var (
@@ -41,6 +42,7 @@ func main() {
 	if err != nil {
 		os.Exit(1)
 	}
+	log.SetupLogging(args.Verbose)
 
 	// Open a JMX connection
 	if err := jmxOpenFunc(args.JmxHost, args.JmxPort, args.JmxUser, args.JmxPass); err != nil {
@@ -88,7 +90,31 @@ func main() {
 
 	jmxCloseFunc()
 
+	jmxIntegration.Entities = checkMetricLimit(jmxIntegration.Entities)
+
 	if err := jmxIntegration.Publish(); err != nil {
 		os.Exit(1)
 	}
+}
+
+// checkMetricLimit looks through all of the metric sets for every entity and aggregates the number
+// of metrics. If that total is greate than args.MetricLimit a warning is logged
+func checkMetricLimit(entities []*integration.Entity) []*integration.Entity {
+	validEntities := make([]*integration.Entity, 0, len(entities))
+
+	for _, entity := range entities {
+		metricCount := 0
+		for _, metricSet := range entity.Metrics {
+			metricCount += len(metricSet.Metrics)
+		}
+
+		if args.MetricLimit != 0 && metricCount > args.MetricLimit {
+			log.Warn("Domain '%s' has %d metrics, the current limit is %d. This Domain will not be reported", entity.Metadata.Name, metricCount, args.MetricLimit)
+			continue
+		}
+
+		validEntities = append(validEntities, entity)
+	}
+
+	return validEntities
 }
--- src/jmx_test.go
+++ src/jmx_test.go
@@ -2,9 +2,48 @@ package main
 
 import (
 	"testing"
+
+	"github.com/newrelic/infra-integrations-sdk/data/metric"
+	"github.com/newrelic/infra-integrations-sdk/integration"
 )
 
-// Insert here the logic for your tests
-func TestPlaceholder(t *testing.T) {
-	t.Skipped()
+func Test_checkMetricList(t *testing.T) {
+	// Ensure we hit the
+	args.MetricLimit = 2
+
+	i, err := integration.New("kafka", "1.0.0")
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err.Error())
+		t.FailNow()
+	}
+
+	e1, err := i.Entity("test_1", "domain")
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err.Error())
+		t.FailNow()
+	}
+
+	set1 := e1.NewMetricSet("testSample")
+	set1.SetMetric("new_metric", 4, metric.GAUGE)
+
+	e2, err := i.Entity("test_2", "domain")
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err.Error())
+		t.FailNow()
+	}
+
+	set2 := e2.NewMetricSet("testSample")
+	set2.SetMetric("new_metric", 4, metric.GAUGE)
+	set2.SetMetric("other_metric", 5, metric.GAUGE)
+
+	out := checkMetricLimit(i.Entities)
+
+	if length := len(out); length != 1 {
+		t.Errorf("Expected 1 entity got %d", length)
+		t.FailNow()
+	}
+
+	if out[0] != e1 {
+		t.Errorf("Expected entity '%+v' got '%+v'", e1, out[0])
+	}
 }
